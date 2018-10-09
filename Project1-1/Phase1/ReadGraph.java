5b396ad509c15176c738ae7d9a241a535dc09a0a
Tu Anh Dinh <dinhtuanh23@gmail.com>
2018-10-09 21:04:53 +0200

    Tu Anh recovered ReadGraph.java

--- Project1-1/Phase1/ReadGraph.java
+++ Project1-1/Phase1/ReadGraph.java
@@ -120,84 +120,149 @@ public class ReadGraph
 			//! e[m-1] will be the last edge
 			//! 
 			//! there will be n vertices in the graph, numbered 1 to n
-
 			//! INSERT YOUR CODE HERE!
 			
-		     hasnoNodes(n);
-			hasNoEdge(m);
-			if (oddCycle(n,e,m)) {
-				System.out.println("This is an odd cycle.");
-			} else{
-				System.out.println("This is not an odd cycle.");
-			}
 			//EXCEPTIONS GO HERE
+			
+			//if (EXCEPTIONS...) then sout("Chromatic number is...")
+			//Else do the main codes 
+
+
+			//MAIN CODE
+			int[] nodes = new int[n];
+			//Loop that counts the number of times a node appears in the set of edges
+			for(int i=0;i<m;i++){
+				nodes[e[i].u-1]++;
+				nodes[e[i].v-1]++;
+			}
+			//Maximum is set to the minimum number of edges possible, which is 0
+			int maximum = 0;
+			//Returns the number of edges for the node that has the highest number of them
+			for(int i=0;i<n;i++){
+				maximum = Math.max(maximum,nodes[i]);
+			}
+			System.out.println("The maximum is: "+maximum);
+			
+			//BRUTE-FORCE GOES HERE
+			
+			
+			
+				
+			
+		}
+
+		//EXCEPTIONS - METHODS
+		/*
+		A bipartite is a graph whose vertices can be divided into two disjoint and independent sets 
+		U and V such that every edge connects a vertex in U to one in V
+		*/
+		public static boolean isBipartite(int numberOfVertice, ColEdge e[]) {
+
+			//Choose 1 fixed vertex to find set U
+			int fixVertex1 = e[0].u;
+			//Create an array for set U - an array that stores other vertices that are not linked to the first fixed vetex
+			int[] setU = new int[numberOfVertice];
+			int setULength = 0;
+
+			//Check every vertex to see if it is linked to the first vertex, add it to set U if it is not linked.
+			for (int i = 1; i <= numberOfVertice; i++) {
+				boolean isLinked = false;
+				for (int j = 0; j < e.length; j++) {
+					if ((e[j].u == fixVertex1 && e[j].v == i) || (e[j].v == fixVertex1 && e[j].u == i)) {
+						isLinked = true;
+						break;
+					}
+				}
+
+				if (!isLinked) {
+					setU[setULength] = i;
+					setULength++;
+				}
+			}
+
+			//Choose 1 fixed vertex (not in set U) to create set V
+			int fixVertex2 = 0;
+			boolean hasFound = false;
+			int index = 1;
+			//Check all vertices to see if there is any vertex that is not in set U
+			while(!hasFound && index <= numberOfVertice) {
+				if (!findNumber(index, setU)) {
+					hasFound = true;
+					fixVertex2 = index;
+				} else {
+					index++;
+				}
+			}
+			//If there is no other vetex that is not linked to the first fixed vertex, then the graph is Bipartite
+			if (!hasFound) {
+				return true;
+			}
+			//If there is a vertex that is not in set U...
+			int[] setV = new int[numberOfVertice];
+			int setVLength = 0;
+
+			for (int i = 1; i <= numberOfVertice; i++) {
+				boolean isLinked = false;
+				for (int j = 0; j < e.length; j++) {
+					if ((e[j].u == fixVertex2 && e[j].v == i) || (e[j].v == fixVertex2 && e[j].u == i)) {
+						isLinked = true;
+						break;
+					}
+				}
+
+				if (!isLinked) {
+					setV[setVLength] = i;
+					setVLength++;
+				}
+			}
+
+
+			boolean isBipartite = true;
+			//If there is a vertex that is not in U and not in V, then the graph is not bipartite
+			for (int i = 1; i <= numberOfVertice; i++) {
+				if (!findNumber(i, setU) && !findNumber(i, setV)) {
+					isBipartite = false;
+				}
+			}
+
+			return isBipartite;
+		}
+
+		public static boolean findNumber(int number, int[] array){
+			boolean findNumber = false;
+			int index = 0;
+			while (!findNumber && index < array.length) {
+				if (array[index] == number) {
+					findNumber = true;
+				} else {
+					index++;
+				}
+			}
+			return findNumber;
+		}
+
+		//A complete graph is a graph in which every pair of distinct vertices is connected by a unique edge
+		public static boolean isCompleteGraph(int numberOfVertice, ColEdge e[]) {
+			boolean isCompleteGraph = true;
+
+			//Check every vertex to see if it is linked to all other vertices
+			for (int i = 1; i <= numberOfVertice; i++) {
+				for (int j = i + 1; j <= numberOfVertice; j++) {
+					boolean hasAllLinked = false;
+					for (int k = 0; k < e.length; k++) {
+						if ((e[k].u == i && e[k].v == j) || (e[k].v == i && e[k].u == j)) {
+							hasAllLinked = true;
+						}
+					}
+					if (!hasAllLinked) {
+						isCompleteGraph = false;
+						break;
+					}
+				}
+			}
+
+			return isCompleteGraph;
+		}
 
 
 }
-			
-		public static boolean hasnoNodes(int n)//n = vertices
-	{	//if there is no vertices then chromatic number is 0 and graph does not exits.
-		
-		
-		if(n == 0)
-		{         
-			
-			//System.out.println("this graph doesnt exist");
-			return true;
-		}
-		//System.out.println("this graph  exist");
-
-		return false;
-	}
-	
-	
-	public static boolean hasNoEdge(int m )
-	{
-		//m == number of edges
-		//if no edges then chromatic number is zero
-		if(m==0)
-		{
-			return true;
-		}
-	
-	return false;
-	}
-	
-	 
-	/**
-	 a cycle is a path with the same first and last vertex.
-	 and length of the cycle equal to number of edges that contains
-	 and a cycle is odd if it contains an odd number of edges
-	 and chromatic number of oddCycle graph is 3.
-	*/
-	
-	public static boolean oddCycle(int n, ColEdge e[],int m) {
-		//n == no.of vertices ,, m == number of edges
-		boolean cyclic = true;
-		
-		//if number of vertices is even return false;
-		if(n%2==0) return false;
-		
-		if(m!=n)return false;
-		
-		 for(int i = 1;i<=n;i++)
-		 {	
-		 	 int count =0;
-		 	 for(int j = 0;j<e.length;j++)
-		 	 {	
-		 	 	 
-		 	 	 if(e[j].u==i||e[j].v==i)
-		 	 	 {
-		 	 	 	 count++;
-		 	 	 }
-		 	 }
-		 	
-		 	 if(count!=2)
-		 	 {
-		 	 	 cyclic = false;
-		 	 }
-		 }
-		 return cyclic;
-	}
-		
-}			
