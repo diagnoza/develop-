5a9e9198081b0104f259db267bd9081d54cc34c1
Michal Jarski <Michal@Stairway-to-Heaven.local>
2018-11-28 15:21:15 +0100

    Fixed the random graph generator and maximum edges dependent on number of vertices

--- Project1-1/Phase2/Project/Windows/RandomOrder.java
+++ Project1-1/Phase2/Project/Windows/RandomOrder.java
@@ -20,22 +20,39 @@ public class RandomOrder {
         boolean[][] graph = new boolean[vertices][vertices];
         int randomParameter;
         int randomParameter2;
-	int edgesCounter = 0;
-        for (int i = 0; i < vertices; i++) {
-            do {
+        int edgesCounter = 0;
+        if(edges<2*vertices){
+            for (int i = 0; i < vertices; i++) {
+                do {
+                    randomParameter = rand.nextInt(vertices);
+                } while (randomParameter == i);
+	        if(!graph[i][randomParameter]) edgesCounter++;
+                graph[i][randomParameter] = true;
+                graph[randomParameter][i] = true;
+            }
+            while (edgesCounter < edges) {
                 randomParameter = rand.nextInt(vertices);
-            } while (randomParameter == i);
-	    if(!graph[i][randomParameter]) edgesCounter++;
-            graph[i][randomParameter] = true;
-            graph[randomParameter][i] = true;
+                randomParameter2 = rand.nextInt(vertices);
+                if (randomParameter2 != randomParameter && graph[randomParameter][randomParameter2] != true) {
+                    graph[randomParameter][randomParameter2] = true;
+                    graph[randomParameter2][randomParameter] = true;
+            edgesCounter++;
+                }
+            }
         }
-        while (edgesCounter < edges) {
-            randomParameter = rand.nextInt(vertices);
-            randomParameter2 = rand.nextInt(vertices);
-            if (randomParameter2 != randomParameter && graph[randomParameter][randomParameter2] != true) {
-                graph[randomParameter][randomParameter2] = true;
-                graph[randomParameter2][randomParameter] = true;
-		edgesCounter++;
+        else{
+            edgesCounter = vertices*(vertices-1)/2;
+            for(int i=0;i<vertices;i++)
+                for(int j=0;j<vertices;j++)
+                    if(i!=j) graph[i][j]=true;
+            while (edgesCounter > edges) {
+                randomParameter = rand.nextInt(vertices);
+                randomParameter2 = rand.nextInt(vertices);
+                if (graph[randomParameter][randomParameter2] != false) {
+                    graph[randomParameter][randomParameter2] = false;
+                    graph[randomParameter2][randomParameter] = false;
+            edgesCounter--;
+                }
             }
         }
         return graph;
