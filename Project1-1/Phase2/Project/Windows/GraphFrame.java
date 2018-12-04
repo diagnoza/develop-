74812e4b6e3fcc721f8245cb2ec38122d2848bb0
Michal Jarski <michal@jarski.eu>
2018-12-04 12:32:36 +0100

    Graph coloring, still without comparing against boolean array of connections

--- Project1-1/Phase2/Project/Windows/GraphFrame.java
+++ Project1-1/Phase2/Project/Windows/GraphFrame.java
@@ -5,7 +5,9 @@ import java.awt.event.*;
 import javax.swing.*; 
 
 public class GraphFrame{
-    public static final JButton[] colors = new JButton[20];
+    public static int colorIndex = 0;
+    public static int[] colorArray = new int[20];
+    public static final JButton[] colors = new JButton[21];
     public static GraphDisplay componentGraph;
     public static final RandomOrder test = new RandomOrder(SecondFrame.z,SecondFrame.y);
     public static boolean[][] test2 = test.createGraph();
@@ -15,7 +17,7 @@ public class GraphFrame{
         graphWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         graphWindow.setSize(1000,600);
         
-        componentGraph = new GraphDisplay(true);
+        componentGraph = new GraphDisplay();
         JPanel graphPanel = new JPanel(new BorderLayout());
 
         JPanel leftMenuPanel = new JPanel();
@@ -40,34 +42,36 @@ public class GraphFrame{
 
         Dimension buttonSize = new Dimension(30,30);
 
-        for(int i=0;i<20;i++){
+        for(int i=0;i<21;i++){
             colors[i] = new JButton();
             colors[i].setOpaque(true);
             colors[i].setBorderPainted(false);
             colors[i].setPreferredSize(buttonSize);
+            colors[i].putClientProperty("index", i);
         }
         
-        colors[0].setBackground(new Color(230,25,75));
-        colors[1].setBackground(new Color(60,180,75));
-        colors[2].setBackground(new Color(255,225,25));
-        colors[3].setBackground(new Color(0,130,200));
-        colors[4].setBackground(new Color(245,130,48));
-        colors[5].setBackground(new Color(145,30,180));
-        colors[6].setBackground(new Color(70,240,240));
-        colors[7].setBackground(new Color(240,50,230));
-        colors[8].setBackground(new Color(210,245,60));
-        colors[9].setBackground(new Color(250,190,190));
-        colors[10].setBackground(new Color(0,128,128));
-        colors[11].setBackground(new Color(230,190,255));
-        colors[12].setBackground(new Color(170,110,40));
-        colors[13].setBackground(new Color(255,250,200));
-        colors[14].setBackground(new Color(128,0,0));
-        colors[15].setBackground(new Color(170,255,195));
-        colors[16].setBackground(new Color(128,128,0));
-        colors[17].setBackground(new Color(255,215,180));
-        colors[18].setBackground(new Color(0,0,128));
-        colors[19].setBackground(new Color(128,128,0));
-        for(int i=0;i<20;i++)
+        colors[0].setBackground(Color.BLACK);
+        colors[1].setBackground(new Color(230,25,75));
+        colors[2].setBackground(new Color(60,180,75));
+        colors[3].setBackground(new Color(255,225,25));
+        colors[4].setBackground(new Color(0,130,200));
+        colors[5].setBackground(new Color(245,130,48));
+        colors[6].setBackground(new Color(145,30,180));
+        colors[7].setBackground(new Color(70,240,240));
+        colors[8].setBackground(new Color(240,50,230));
+        colors[9].setBackground(new Color(210,245,60));
+        colors[10].setBackground(new Color(250,190,190));
+        colors[11].setBackground(new Color(0,128,128));
+        colors[12].setBackground(new Color(230,190,255));
+        colors[13].setBackground(new Color(170,110,40));
+        colors[14].setBackground(new Color(255,250,200));
+        colors[15].setBackground(new Color(128,0,0));
+        colors[16].setBackground(new Color(170,255,195));
+        colors[17].setBackground(new Color(128,128,0));
+        colors[18].setBackground(new Color(255,215,180));
+        colors[19].setBackground(new Color(0,0,128));
+        colors[20].setBackground(new Color(128,128,0));
+        for(int i=1;i<21;i++)
             colorPickerPanel.add(colors[i]);
         leftMenuPanel.add(colorPickerPanel);
 
@@ -98,9 +102,10 @@ public class GraphFrame{
                 JButton bl = (JButton) e.getSource();
                 currentColor.setBackground(bl.getBackground());
                 currentColor.setForeground(bl.getBackground());
+                colorIndex = (int) bl.getClientProperty("index");
             }
         }
-        for(int i=0;i<20;i++)
+        for(int i=1;i<21;i++)
             colors[i].addActionListener(new ButtonListener());
     }
 }
